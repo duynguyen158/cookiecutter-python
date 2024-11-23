20582ec68e20c313ad903292aa6231087cd0b75a
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2024-11-23 03:01:04 -0500

    [add] delete license if not open-source

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,8 +4,12 @@
 # Infrastructure-as-code
 mkdir -p infrastructure/environments/prod/main.tf
 mkdir -p infrastructure/modules/example/main.tf
-
 {% endif %}
+
+{% if cookiecutter.open_source_license == "Not open source" %}
+rm LICENSE
+{% endif %}
+
 echo "🎉🎉🎉 Project created! 🎉🎉🎉"
 echo "You can add the following alias to your .zshrc or .bashrc to run commands installed in compose.yaml (such as uv) more easily:"
 echo ""
